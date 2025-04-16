d3519c82a1ec546d6f4d2dabe0d9a9f152728329
Awais Arjumand <awaisarjumand557@gmail.com>
2025-04-16 13:43:33 +0500

    fix blog image

--- src/pages/blogs/manage-blog/index.tsx
+++ src/pages/blogs/manage-blog/index.tsx
@@ -20,13 +20,13 @@ const ManageBlog = () => {
       <div className="flex flex-col gap-6 md:gap-4">
         <div className="h-64 w-full border-2 border-dotted border-gray-300 bg-white rounded-xl flex flex-col gap-5 py-10  justify-center items-center  ">
           <img
-            src="/images/students/enrolled-students.svg"
+            src="/images/blog/blogupload.svg"
             className="h-20 w-20"
           />
-          <p className="font-medium text-base">
+          <p className="font-medium text-base !font-family-barlow">
             Drag and drop your files here or click to
           </p>
-          <Button className="w-fit " variant="secondary">
+          <Button className="w-fit !font-family-barlow" variant="secondary">
             Browse File
           </Button>
         </div>
